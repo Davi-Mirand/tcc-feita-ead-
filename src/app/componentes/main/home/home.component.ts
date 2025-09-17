a1aff9fdf4b87e1b2d7a3e50c68cabd4cd49da18
o <nascimento.luiz322@gmail.com>
2025-09-17 00:57:20 -0300

    atualizações home lulu (não finalizado)

--- src/app/componentes/main/home/home.component.ts
+++ src/app/componentes/main/home/home.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-import { CarrouselComponent } from "../../carrousel/carrousel.component";
+import { CarrouselComponent } from '../../carrousel/carrousel.component';
 
 @Component({
   selector: 'app-home',
+  standalone: true, // 👈 adiciona isso
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
   imports: [CarrouselComponent]
 })
-export class HomeComponent {
-  
-}
+export class HomeComponent {}
